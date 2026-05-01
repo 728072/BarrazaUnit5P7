427b699fa390db7cc3ac5f8d5156893767bbb23b
728072 <728072@student.dusd.net>
2026-05-01 15:24:28 -0700

    5.1

---
+++ P7BarrazaUnit5New/Assets/Course Library/Scripts/Target.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+public class Target : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    private float minSpeed = 12;
+    private float maxSpeed = 16;
+    private float maxTorque = 10;
+    private float xRange = 4;
+    private float yRange = -6;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        rb.AddForce(RandomForce(), ForceMode.Impulse);
+        rb.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse);
+
+        transform.position = RandomSpawn();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        Destroy(gameObject);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+    }
+
+    Vector3 RandomForce()
+    {
+        return Vector3.up * Random.Range(minSpeed, maxSpeed);
+    }
+
+    float RandomTorque()
+    {
+        return Random.Range(-maxTorque, maxTorque);
+    }
+
+    Vector3 RandomSpawn()
+    {
+        return new Vector3(Random.Range(-xRange, xRange), yRange);
+    }
+}
