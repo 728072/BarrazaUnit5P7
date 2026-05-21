4f5c24d05d23a97e5b48f0f8459c70b1fe61a893
728072 <728072@student.dusd.net>
2026-05-21 15:10:49 -0700

    challenge 5 done

---
+++ P7BarrazaUnit5New/Assets/Challenge 5/Scripts/DifficultyButtonX.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DifficultyButtonX : MonoBehaviour
+{
+    private Button button;
+    private GameManagerX gameManagerX;
+    private TargetX targetX;
+    public int difficulty;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameManagerX = GameObject.Find("Game Manager").GetComponent<GameManagerX>();
+        targetX = GameObject.Find("Game Manager").GetComponent<TargetX>();
+        button = GetComponent<Button>();
+        button.onClick.AddListener(SetDifficulty);
+    }
+
+    /* When a button is clicked, call the StartGame() method
+     * and pass it the difficulty value (1, 2, 3) from the button 
+    */
+    void SetDifficulty()
+    {
+        Debug.Log(button.gameObject.name + " was clicked");
+        gameManagerX.StartGame(difficulty);
+        targetX.ScreenTime(difficulty);
+    }
+
+
+
+}
